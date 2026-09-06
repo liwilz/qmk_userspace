e47181e6f77383227ae08a8d293db01d06061aff
Will <wys24@ic.ac.uk>
2026-09-06 17:36:52 +0100

    added actual keymap

---
+++ keyboards/splitkb/keymaps/liwilz/config.h
@@ -0,0 +1,10 @@
+// Copyright 2024 splitkb.com (support@splitkb.com)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Myriad boilerplate
+#define MYRIAD_ENABLE
+
+// Alternative tapping term
+#define TAPPING_TERM 200
